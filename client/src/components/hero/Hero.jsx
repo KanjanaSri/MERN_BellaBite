ec2ab9b276bb136bf49cf8770f67b6efe7c2edd1
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-12-04 22:38:39 -0800

    add hero section ui

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import classes from "./hero.module.css";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import lunchBreak from "../../assets/lunch_break.svg";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section
+      style={{ height: "200vh" }}
+      id="home"
+      className={classes.container}
+    >
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <h2 className={classes.title}>Explore Our Culinary Wonders</h2>
+          <p className={classes.firstMsg}>
+            Whether the comfort of <span>home-style</span> cooking
+          </p>
+          <p className={classes.secondMsg}>
+            or the <span>exotic</span> flavors, our diverse menu promises to{" "}
+            <span>satisfy</span> every palate.
+          </p>
+          <p className={classes.desc}>
+            Step into a world where passion for food meets exceptional culinary
+            craftsmanship.
+          </p>
+          <div className={classes.buttons}>
+            <button className={classes.buttonOrder}>Order now!</button>
+            <button className={classes.buttonSee}>
+              <a href="#foods">
+                See what's available <AiOutlineArrowDown />{" "}
+              </a>
+            </button>
+          </div>
+        </div>
+
+        <div className={classes.right}>
+          <img src={lunchBreak} alt="" className={classes.manEatingImg} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import classes from "./navbar.module.css";
+import { FiUser, FiShoppingCart } from "react-icons/fi";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <div className={`${classes.container} ${isScrolled && classes.scrolled}`}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Link to="/" className={classes.title}>
+            Foodable
+          </Link>
+        </div>
+
+        <div className={classes.center}>
+          <ul className={classes.list}>
+            <li className={classes.listItem}>
+              <a href="#">Home</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#contacts">Contacts</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#foods">Foods</a>
+            </li>
+            <li className={classes.listItem}>
+              <a href="#faq">FAQ</a>
+            </li>
+            <li className={classes.listItem}>
+              <Link to="/create">Create</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className={classes.right}>
+          <FiUser className={classes.userIcon} />
+          <Link to="/cart" className={classes.cartContainer}>
+            <FiShoppingCart className={classes.cartIcon} />
+            <div className={classes.cartQuantity}>0</div>
+          </Link>
+
+          <button className={classes.logout}>Logout</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
